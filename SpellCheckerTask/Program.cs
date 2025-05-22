274893ecf687d69556b7ee5275479cc212a73f01
TCross4 <144451394+TCross4@users.noreply.github.com>
2025-05-22 19:48:35 +0100

    Update Program.cs

--- SpellCheckerTask/Program.cs
+++ SpellCheckerTask/Program.cs
@@ -11,6 +11,83 @@ namespace SpellCheckerTask
         static void Main(string[] args)
         {
             string[] words = createDictionary();
+        Console.WriteLine("Input a word.");
+        string word = Console.ReadLine();
+        bool valid = false;
+        int count = 0;
+        int right = 0;
+        int wrong = 0;
+        while (valid == false)
+        {
+            if (dictionayData[count] == word)
+            {
+                valid = true;
+            }
+            count++;
+        }
+        Console.WriteLine(valid);
+        if (valid == false)
+        {
+            wrong++;
+            WordsFile.txt(word);
+        }
+        else
+        {
+            right++
+        }
+        string[] words2 = new string[3];
+        for (int i = 0; i < 2; i++)
+        {
+            Console.WriteLine("Input a word.");
+            words2[i] = Console.ReadLine;
+        }
+        bool valid1 = false;
+        bool valid2 = false;
+        bool valid3 = false;
+        while(valid1 == false && valid2 == false && valid3 == false)
+        {
+            if (dictionayData == words2[0])
+            {
+                valid1 = true;
+            }
+            else if (dictionayData == words2[1])
+            {
+                valid2 = true;
+            }
+            else if (dictionayData == words2[2])
+            {
+                valid3 = true;
+            }
+        }
+        Console.WriteLine(valid1 + valid2 + valid3);
+        if (valid1 == false)
+        {
+            wrong++;
+            WordsFile.txt(words2[0]);
+        }
+        else if (valid2 == false)
+        {
+            wrong++;
+            WordsFile.txt(words2[1]);
+        }
+        else if (valid3 == false)
+        {
+            wrong++;
+            WordsFile.txt(words2[2]);
+        }
+        if (valid1 == true)
+        {
+            right++;
+        }
+        else if (valid2 == true)
+        {
+            right++;
+        }
+        else if (valid3 == true)
+        {
+            right++;
+        }
+        Console.WriteLine(right / (right + wrong) * 100 + "%");
             //1. Take a user input of a word an check if it has been spelled correctly
 
             //2. Take a string of words as a user input and check they have all been spelled correctly
@@ -27,17 +104,14 @@ namespace SpellCheckerTask
             //Add these suggested words to a spelling list that the user can save as a file to work on
             //their own spelling
 
-
-
-        }
-        static string[] createDictionary()
+    }
+    static string[] createDictionary()
         {
             using StreamReader words = new("WordsFile.txt");
             int count = 0;
             string[] dictionaryData = new string[178636];
             while (!words.EndOfStream)
             {
-
                 dictionaryData[count] = words.ReadLine();
                 count++;
             }
